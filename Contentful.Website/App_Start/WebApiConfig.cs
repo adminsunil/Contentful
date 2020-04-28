7e02a5fa5ee6f25d410ab36af80a64f0b75cfde7
Bhuvaneshwar Solanki <bsolanki@rightpoint.com>
2020-04-28 11:07:48 +0530

    implement CORS

--- Contentful.Website/App_Start/WebApiConfig.cs
+++ Contentful.Website/App_Start/WebApiConfig.cs
@@ -1,29 +1,30 @@
-﻿namespace Contentful.Website
+namespace Contentful.Website
 {
-    using System.Linq;
-    using System.Web.Http;
-    using Newtonsoft.Json.Serialization;
+  using System.Linq;
+  using System.Web.Http;
+  using Newtonsoft.Json.Serialization;
 
-    public static class WebApiConfig
+  public static class WebApiConfig
+  {
+    public static void Register(HttpConfiguration config)
     {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+      config.EnableCors();
+      // Web API configuration and services
 
-            // Web API routes
-            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(
+      // Web API routes
+      config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(
                 config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml"));
 
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+      config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
 
-            config.MapHttpAttributeRoutes();
+      config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
+      config.Routes.MapHttpRoute(
+          name: "DefaultApi",
+          routeTemplate: "api/{controller}/{id}",
+          defaults: new { id = RouteParameter.Optional }
+      );
     }
+  }
 }
--- Contentful.Website/Controllers/HomeController.cs
+++ Contentful.Website/Controllers/HomeController.cs
@@ -1,54 +1,57 @@
-﻿using System.Linq;
+using System.Linq;
 
 namespace Contentful.Website.Controllers
 {
-    using System;
-    using System.Threading.Tasks;
-    using System.Web.Http;
-    using Contentful.API.Services;
+  using System;
+  using System.Threading.Tasks;
+  using System.Web.Http;
+  using System.Web.Http.Cors;
+  using Contentful.API.Services;
 
-    [RoutePrefix("contentful")]
-    public class HomeController : ApiController
+  [RoutePrefix("contentful")]
+  [EnableCors(origins: "*", headers: "*", methods: "*")]
+  public class HomeController : ApiController
+  {
+    private readonly IHomePageService _homePageService;
+    private readonly IBlogPageService _blogPageService;
+    private readonly IServicesPageService _servicesPageService;
+    public HomeController(IHomePageService homePageService, IBlogPageService blogPageService, IServicesPageService servicesPageService)
     {
-        private readonly IHomePageService _homePageService;
-        private readonly IBlogPageService _blogPageService;
-        private readonly IServicesPageService _servicesPageService;
-        public HomeController(IHomePageService homePageService, IBlogPageService blogPageService, IServicesPageService servicesPageService)
-        {
-            _homePageService = homePageService ?? throw new ArgumentNullException(nameof(homePageService));
-            _blogPageService = blogPageService ?? throw new ArgumentNullException(nameof(blogPageService));
-            _servicesPageService = servicesPageService ?? throw new ArgumentNullException(nameof(servicesPageService));
-        }
-        [Route("home-page")]
-        [HttpGet]
-        public async Task<IHttpActionResult> HomePage()
-        {
-            var homePage = await _homePageService.GetHomePage();
-            return Ok(homePage);
-        }
-
-        [Route("blog-page")]
-        [HttpGet]
-        public async Task<IHttpActionResult> BlogPage()
-        {
-            var blogList = await _blogPageService.GetBlogList();
-            return Ok(blogList);
-        }
-
-        [Route("blog-detail")]
-        [HttpGet]
-        public async Task<IHttpActionResult> BlogDetail(string slug)
-        {
-            var blogDetail = await _blogPageService.GetBlogDetail(slug);
-            return Ok(blogDetail);
-        }
-
-        [Route("services-page")]
-        [HttpGet]
-        public async Task<IHttpActionResult> ServicesPage()
-        {
-            var serviceList = await _servicesPageService.GetServiceList();
-            return Ok(serviceList);
-        }
+      _homePageService = homePageService ?? throw new ArgumentNullException(nameof(homePageService));
+      _blogPageService = blogPageService ?? throw new ArgumentNullException(nameof(blogPageService));
+      _servicesPageService = servicesPageService ?? throw new ArgumentNullException(nameof(servicesPageService));
     }
+
+    [Route("home-page")]
+    [HttpGet]
+    public async Task<IHttpActionResult> HomePage()
+    {
+      var homePage = await _homePageService.GetHomePage();
+      return Ok(homePage);
+    }
+
+    [Route("blog-page")]
+    [HttpGet]
+    public async Task<IHttpActionResult> BlogPage()
+    {
+      var blogList = await _blogPageService.GetBlogList();
+      return Ok(blogList);
+    }
+
+    [Route("blog-detail")]
+    [HttpGet]
+    public async Task<IHttpActionResult> BlogDetail(string slug)
+    {
+      var blogDetail = await _blogPageService.GetBlogDetail(slug);
+      return Ok(blogDetail);
+    }
+
+    [Route("services-page")]
+    [HttpGet]
+    public async Task<IHttpActionResult> ServicesPage()
+    {
+      var serviceList = await _servicesPageService.GetServiceList();
+      return Ok(serviceList);
+    }
+  }
 }
